914c9991a8db232275980452a662a482e997fdaf
NarayanaReddy <47046208+NarayanaReddyAnnapureddy@users.noreply.github.com>
2019-06-21 12:07:26 +0530

    Initial Commit

---
+++ src/main/java/com/narayana/spring/cloud/pcf/ServiceRegistryPcfApplication.java
@@ -0,0 +1,23 @@
+package com.narayana.spring.cloud.pcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class ServiceRegistryPcfApplication {
+
+	@GetMapping("/")
+	public String getMessage() {
+		return "Welcome to NTT Data ....";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryPcfApplication.class, args);
+	}
+
+}
